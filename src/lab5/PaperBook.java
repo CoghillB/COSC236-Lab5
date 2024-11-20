686cafff9c87f44eaa41c8b124b3f3151512d477
Brayden <coghill30@gmail.com>
2024-11-19 16:32:57 -0800

    Refactor to use PaperBook instead of Book
    Replaced all instances of Book with PaperBook across the project to ensure consistency and implemented the Book interface in PaperBook. Revised relevant test cases, controllers, and library methods to reflect these changes.

---
+++ src/lab5/PaperBook.java
@@ -0,0 +1,32 @@
+package lab5;
+
+public class PaperBook implements Book {
+    //implements interface Book
+    private String title;
+    private boolean isAvailable;
+
+    public PaperBook(String title) {
+        this.title = title;
+        this.isAvailable = true;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    @Override
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public boolean getIsAvailable() {
+        return isAvailable;
+    }
+}
--- src/lab5/tests/AllTests.java
+++ src/lab5/tests/AllTests.java
@@ -1,18 +1,16 @@
 package lab5.tests;
 
 import org.junit.platform.suite.api.SelectClasses;
-import org.junit.platform.suite.api.Suite;
-
+import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
-@SelectClasses({TestAddRemoveBooks.class, 
-				TestAddRemoveMembers.class,
-				TestFindClassString.class,
-				TestBorrowBooks.class,
-				TestNotAvailableBook.class,
-				TestRemoveMemberWithBooks.class,
-				TestController.class})
+@SelectClasses({TestAddRemoveBooks.class,
+        TestAddRemoveMembers.class,
+        TestFindClassString.class,
+        TestBorrowBooks.class,
+        TestNotAvailablePaperBook.class,
+        TestRemoveMemberWithBooks.class,
+        TestController.class})
 public class AllTests {
     // No need for test methods here; the suite will run the included classes
 }
-
